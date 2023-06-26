01fd692f33cd5589280af1f5237fa014d370093d
MahzuzH <mahzuzhazman@gmail.com>
2023-06-26 14:25:49 +0700

    edit icon

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
 
                     <div className="contact__info">
                         <div className="contact__card">
-                            <i className="bx bxl-mail-send contact__card-icon"></i>
+                            <i className="bx bx-mail-send contact__card-icon"></i>
 
                             <h3 className="contact__card-title">Email</h3>
                             <span className="contact__card-data">
@@ -29,7 +29,7 @@ const Contact = () => {
                         </div>
 
                         <div className="contact__card">
-                            <i className="bx bx;-whatsapp contact__card-icon"></i>
+                            <i className="bx bxl-whatsapp contact__card-icon"></i>
 
                             <h3 className="contact__card-title">Whatsapp</h3>
                             <span className="contact__card-data">
