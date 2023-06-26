2ff658b15122f9ffa5e215ef7c0f132b337dc639
MahzuzH <mahzuzhazman@gmail.com>
2023-06-27 01:13:21 +0700

    update footer css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -7,7 +7,7 @@ const Footer = () => {
             <div className="footer__container container">
                 <h1 className="footer__title">Mahzuz</h1>
 
-                <ul className="footerl__ist">
+                <ul className="footer__list">
                     <li>
                         <a href="#about" className="footer_link">
                             About
