d94f0d06737b20cfd13c1f42c90d3d4a44669ae2
MahzuzH <mahzuzhazman@gmail.com>
2023-06-27 00:39:33 +0700

    add emailjs function

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
+import emailjs from "@emailjs/browser";
 import "./contact.css";
 
 const Contact = () => {
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs
+            .sendForm(
+                "service_yad7w1h",
+                "service_yad7w1h",
+                form.current,
+                "Kp6B3QOqZ1RT60Exi"
+            )
+            .then(
+                (result) => {
+                    console.log(result.text);
+                },
+                (error) => {
+                    console.log(error.text);
+                }
+            );
+    };
     return (
         <section className="contact section" id="contact">
             <h2 className="section__title">Get in touch</h2>
@@ -51,7 +73,11 @@ const Contact = () => {
                 <div className="contact__content">
                     <h3 className="contact__title">Write me your project</h3>
 
-                    <form action="" className="contact__form">
+                    <form
+                        ref={form}
+                        onSubmit={sendEmail}
+                        className="contact__form"
+                    >
                         <div className="contact__form-div">
                             <label className="contact__form-tag">Name</label>
                             <input
