c0a8eeb5a72d73dbaf6d5c0f5e5389b2994b5070
MahzuzH <mahzuzhazman@gmail.com>
2023-06-26 13:55:45 +0700

    add contact

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import './App.css';
-import Header from './components/header/Header';
-import Home from './components/home/Home';
-import About from './components/about/About';
-import Skills from './components/skills/Skills';
-import Qualification from './components/qualification/Qualification';
+import "./App.css";
+import Header from "./components/header/Header";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Skills from "./components/skills/Skills";
+import Qualification from "./components/qualification/Qualification";
+import Contact from "./components/contact/Contact";
 
 function App() {
-  return (
-    <>  
-      <Header />
-      <main className='main'>
-        <Home />
-        <About />
-        <Skills />
-        <Qualification/>
-      </main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main className="main">
+                <Home />
+                <About />
+                <Skills />
+                <Qualification />
+                <Contact />
+            </main>
+        </>
+    );
 }
 
 export default App;
