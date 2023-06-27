188939840f2b252222a2fc0a38f32e650e381f3d
MahzuzH <mahzuzhazman@gmail.com>
2023-06-27 23:45:05 +0700

    change image

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./about.css";
-import AboutImg from "../../assets/mjd_crop.jpg";
+import AboutImg from "../../assets/sdk.jpg";
 import CV from "../../assets/CV--MahzuzHazman.pdf";
 import Info from "./Info";
 
