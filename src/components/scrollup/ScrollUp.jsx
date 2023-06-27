eadb5be98db7fffe565251128e173c2b0933c312
MahzuzH <mahzuzhazman@gmail.com>
2023-06-27 12:23:53 +0700

    update scrollup

--- src/components/scrollup/ScrollUp.jsx
+++ src/components/scrollup/ScrollUp.jsx
@@ -3,7 +3,7 @@ import "./scrollup.css";
 
 const ScrollUp = () => {
     window.addEventListener("scroll", function () {
-        const scrollup = this.document.querrySelector(".scrollup");
+        const scrollup = document.querySelector(".scrollup");
         if (this.scrollY >= 560) scrollup.classList.add("show-scroll");
         else scrollup.classList.remove("show-scroll");
     });
